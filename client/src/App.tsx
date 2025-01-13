60979e2fe521ee8438985fa058a25bdc488b87c4
Pavan Kumar Bandaru <71021764+pth55@users.noreply.github.com>
2025-01-13 19:53:52 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -59,7 +59,7 @@ function App() {
       // Construct request body
       const body = file ? formData : JSON.stringify({ text: prompt });
 
-      const response = await fetch("http://127.0.0.1:5000/process", {
+      const response = await fetch("https://piracy-text-be.onrender.com/process", {
         method: "POST",
         headers: headers,
         body: body
