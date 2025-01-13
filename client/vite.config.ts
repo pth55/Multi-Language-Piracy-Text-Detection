64479828fff19f988d3f5b9123f1b1f12ba404d9
Pavan Kumar Bandaru <71021764+pth55@users.noreply.github.com>
2025-01-13 20:22:01 +0530

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['pdfjs-dist/build/pdf.worker.mjs'],
-  },
   build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          pdfWorker: ['pdfjs-dist/build/pdf.worker.mjs'],
-        },
-      },
-    },
+    outDir: 'dist',
+    sourcemap: true,
   },
 });
